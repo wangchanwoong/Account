34a3aae50988d19d8fb42c98c2c870a5324fb5a9
WangChanWoong <cksdnd1232@gmail.com>
2022-12-29 22:50:10 +0900

    Initial commit

---
+++ src/main/java/com/example/account/dto/QueryTransactionResponse.java
@@ -0,0 +1,35 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QueryTransactionResponse {
+        private TransactionResultType transactionResultType;
+        private String accountNumber;
+        private LocalDateTime transactedAt;
+        private String transactionId;
+        private Long amount;
+        private TransactionType transactionType;
+
+
+
+        public static QueryTransactionResponse from(TransactionDto transactionDto) {
+            return QueryTransactionResponse.builder()
+                    .accountNumber(transactionDto.getAccountNumber())
+                    .transactionType(transactionDto.getTransactionType())
+                    .transactionResultType(transactionDto.getTransactionResultType())
+                    .transactionId(transactionDto.getTransactionId())
+                    .amount(transactionDto.getAmount())
+                    .transactedAt(transactionDto.getTransactedAt())
+                    .build();
+        }
+}
+
